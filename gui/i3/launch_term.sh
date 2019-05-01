95f54a02b125681cc6ee222d606a494dc3b28944
Travis Finkenauer <tmfinken@gmail.com>
2019-05-01 12:22:28 -0400

    Add terminal launch fallback

---
+++ gui/i3/launch_term.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Launch preferred terminal if possible
+
+TERMS="
+    noexec
+    alacritty
+    gnome-terminal
+    terminator
+    uxterm
+    xterm
+    "
+
+for term in $TERMS; do
+    #"$term" "$@" || echo "Failed to exec $term"
+    if hash "$term" > /dev/null 2>&1 ; then
+        echo "executing $term"
+        exec "$term" "$@"
+    else
+        echo "$term not available"
+    fi
+done
+
+echo "Failed to find suitable terminal"
+exit 1
