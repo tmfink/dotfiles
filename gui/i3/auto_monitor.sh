6fd9d4c2259828b6e5a83f0207396c0083d1b54a
Travis Finkenauer <tmfinken@gmail.com>
2020-07-30 14:19:55 -0400

    Support directions for auto_monitor

--- gui/i3/auto_monitor.sh
+++ gui/i3/auto_monitor.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 #
 # Uses xrandr to detect and setup active outputs
+#
+# direction: left|right|above|below
+#    default: right
 
 set -euo pipefail
 
-########## 
-# Set to name of main display
-MAIN_PATTERN="eDP-1"
 ##########
+# Set to name of main display
+MAIN_MONITOR="${MAIN_MONITOR:-eDP-1}"
+##########
+
+usage() {
+    echo "$0: [-h] [left|right|above|below]"
+    exit 1
+}
+
+for arg in "$@"; do
+    case $arg in
+    -h|--help) usage ;;
+    *) ;;
+    esac
+done
 
 ACTIVE_OUTPUTS=$(xrandr | grep -oE '^.+ connected' | cut -d" " -f1)
 INACTIVE_OUTPUTS=$(xrandr | grep -oE '^.+ disconnected' | cut -d" " -f1)
 NUM_ACTIVE_OUTPUTS=$(echo "$ACTIVE_OUTPUTS" | wc -l)
 
-MAIN=$(echo "$ACTIVE_OUTPUTS" | grep "$MAIN_PATTERN")
+MAIN=$(echo "$ACTIVE_OUTPUTS" | grep "$MAIN_MONITOR")
 
 echo "Found $NUM_ACTIVE_OUTPUTS active outputs"
 echo "Active: $(echo $ACTIVE_OUTPUTS | tr '\n' ' ')"
@@ -22,6 +37,23 @@ echo
 echo "MAIN: $MAIN"
 
 INACTIVE_OUTPUT_CMD=$(for X in $INACTIVE_OUTPUTS; do echo --output $X --off; done)
+
+EXTERNAL_DIR=${1:-right}
+
+case $EXTERNAL_DIR in
+left|right)
+    DIRECTION_ARG="--${EXTERNAL_DIR}-of"
+    ;;
+above|below)
+    DIRECTION_ARG="--${EXTERNAL_DIR}"
+    ;;
+*)
+    echo "Unsupported direction ${EXTERNAL_DIR}" 1>&2
+    exit 1
+    ;;
+esac
+
+
 
 case $NUM_ACTIVE_OUTPUTS in
     0)
@@ -38,7 +70,7 @@ case $NUM_ACTIVE_OUTPUTS in
         EXTERNAL=$(echo "$ACTIVE_OUTPUTS" | grep -v "^${MAIN}$")
         echo "external is $EXTERNAL"
         xrandr $INACTIVE_OUTPUT_CMD --output "$MAIN" --auto --primary \
-            --output "$EXTERNAL" --right-of "$MAIN" --auto
+            --output "$EXTERNAL" "${DIRECTION_ARG}" "$MAIN" --auto
         ;;
     *)
         echo "Unsupported number of active outputs: $NUM_ACTIVE_OUTPUTS"
