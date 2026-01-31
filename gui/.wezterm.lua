545c5b5701667408c34e92c5f62af62fee05ba8f
Travis Finkenauer <tmfinken@gmail.com>
2026-01-30 22:39:11 -0800

    wezterm: avoid selecting tmux panes; add split shortcuts

--- gui/.wezterm.lua
+++ gui/.wezterm.lua
@@ -2,7 +2,6 @@
 
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-local wez = wezterm
 local act = wezterm.action
 
 -- This will hold the configuration.
@@ -14,12 +13,14 @@ config.initial_rows = 28
 
 -- Font
 config.font_size = 12
-config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font = wezterm.font 'FiraMono Nerd Font'
 
 config.window_frame = {
     --font = wez.font({ family = 'PayPal Sans Big' }),
-    font_size = 12,
+    --font_size = 16,
 }
+
+config.audible_bell = "Disabled"
 
 -- disable ligatures
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
@@ -27,6 +28,13 @@ config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.color_scheme = 'Classic Dark (base16)'
 -- Classic Dark (base16) is used in Alacritty
 --config.default_prog = { '/Users/tmfink/.nix-profile/bin/fish', '-l' }
+
+-- box drawing characters are used to draw tmux panes
+local box_drawing_str = ""
+for code = 0x2500, 0x257F do
+  box_drawing_str = box_drawing_str .. utf8.char(code)
+end
+config.selection_word_boundary = " \t\n{}[]()\"'`;:" .. box_drawing_str
 
 config.window_padding = {
     left = 2,
@@ -37,24 +45,86 @@ config.window_padding = {
 
 -- https://wezterm.org/config/keys.html
 -- Show defaults: wezterm show-keys --lua
-local rename_tab_action = act.PromptInputLine {
-    description = 'Enter new name for tab',
-    action = wezterm.action_callback(function(window, _pane, line)
-        -- line will be `nil` if they hit escape without entering anything
-        -- An empty string if they just hit enter
-        -- Or the actual line of text they wrote
-        if line then
-            window:active_tab():set_title(line)
-        end
-    end),
-}
 config.keys = {
+    -- Tabs
     { key = 'LeftArrow',  mods = 'SUPER',       action = act.ActivateTabRelative(-1) },
     { key = 'RightArrow', mods = 'SUPER',       action = act.ActivateTabRelative(1) },
     { key = 'LeftArrow',  mods = 'SHIFT|SUPER', action = act.MoveTabRelative(-1) },
     { key = 'RightArrow', mods = 'SHIFT|SUPER', action = act.MoveTabRelative(1) },
-    { key = 'I', mods = 'SHIFT|SUPER',          action = rename_tab_action },
-    { key = 'I', mods = 'CTRL|SHIFT',           action = rename_tab_action },
+
+    {
+        key = 'I',
+        mods = 'SHIFT|SUPER',
+        action = act.PromptInputLine {
+            description = 'Enter new name for tab',
+            action = wezterm.action_callback(function(window, _pane, line)
+                -- line will be `nil` if they hit escape without entering anything
+                -- An empty string if they just hit enter
+                -- Or the actual line of text they wrote
+                if line then
+                    window:active_tab():set_title(line)
+                end
+            end),
+        },
+    },
+    -- Splits
+    {
+        key = '%',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+    },
+    {
+        key = '"',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+    },
+    {
+        key = '"',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+    },
+    -- Pane focus (arrows)
+    {
+        key = 'LeftArrow',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Left',
+    },
+    {
+        key = 'RightArrow',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Right',
+    },
+    {
+        key = 'UpArrow',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Up',
+    },
+    {
+        key = 'DownArrow',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Down',
+    },
+    -- Pane focus (vi keys)
+    {
+        key = 'h',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Left',
+    },
+    {
+        key = 'l',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Right',
+    },
+    {
+        key = 'k',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Up',
+    },
+    {
+        key = 'j',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Down',
+    },
 }
 
 -- https://www.reddit.com/r/wezterm/comments/10jda7o/is_there_a_way_not_to_open_urls_on_simple_click/
@@ -81,7 +151,7 @@ config.mouse_bindings = {
     {
         event = { Down = { streak = 3, button = 'Left' } },
         action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
-        mods = 'NONE',
+        mods = 'SHIFT',
     },
 }
 
